79db1e40c5fad6ff539953f71da1ad2ebb2f7315
Jordan Curzon <curzonj@gmail.com>
2012-06-19 11:12:02 -0700

    fix chef

--- plugins/agent/chef.rb
+++ plugins/agent/chef.rb
@@ -8,7 +8,13 @@ module MCollective
     #    chef.client   - Where to find the chef client, defaults to /usr/sbin/chef-client
     #    chef.pidfile   - Where to find the chef client pid file
     class Chef<RPC::Agent
-      metadata :timeout => 180
+      metadata :name => 'Chef',
+               :description => "run chef",
+               :author      => "jc816a",
+               :license     => "none",
+               :version     => "", 
+               :url         => "", 
+               :timeout => 180 
 
       action "node_json" do
         reply.data = File.read("/var/tmp/chef_node.json")
